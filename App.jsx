b96343393b01f53721d3fbb658dce4fb80e0505a
Shiddu <30692292+shiddugmail@users.noreply.github.com>
2021-06-15 09:04:56 +0530

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,122 @@
+import "./App.css";
+import React from "react";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import Frame173 from "./components/Frame173";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/:path(|260_12352)">
+          <Frame173 {...frame173Data} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+const group89Data = {
+    line4: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/line-4@1x.svg",
+    line5: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/line-4@1x.svg",
+    line6: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/line-4@1x.svg",
+    line7: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/line-4@1x.svg",
+    line8: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/line-4@1x.svg",
+};
+
+const founderCardData = {
+    rectangle90: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-90@2x.png",
+    name: "Jenny Wilson",
+    designation: "Designation",
+};
+
+const founderCard2Data = {
+    rectangle90: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-90-1@2x.png",
+    name: "Brooklyn Simmons",
+    designation: "Designation",
+};
+
+const founderCard32Data = {
+    rectangle90: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-90-2@2x.png",
+    name: "Esther Howard",
+    designation: "Designation",
+};
+
+const founderCard4Data = {
+    rectangle90: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-90-3@2x.png",
+    name: "Jane Cooper",
+    designation: "Designation",
+};
+
+const frame173Data = {
+    text1: "There are many variations of passages of Lorem Ipsum available but the majority have suffered dummy alteration in that an some form by the injected humour or randomised words which don’t look even that slightly believable. If you are Lorem Ipsums your need to be sure there isn’t anything embarrassing. There are many variations passages of Lorem as Ipsums available but the majority have suffered that alterations in that some form by injected humour or randomised words which don’t looks even slightly that believable. If you are Lorem as Ipsum  you need to be sure there isn’t anything as embarrassing. There are many variation of passages Lorem Ipsum available but the majority have suffered that a aalteration in that some form by injected humour or randomised words which don’t look even slightly the believable.",
+    rectangle76: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-76@1x.png",
+    title: "About Us",
+    whyChooseUs: "Why Choose Us?",
+    founders: "Founders",
+    line55: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/line-55@1x.svg",
+    vector2: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-177@2x.svg",
+    vector3: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-178@2x.svg",
+    place: "Quality",
+    vector4: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-7@2x.svg",
+    vector5: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-182@2x.svg",
+    vector6: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-9@2x.svg",
+    vector7: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-184@2x.svg",
+    vector8: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-11@2x.svg",
+    vector9: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-12@2x.svg",
+    vector10: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-187@2x.svg",
+    vector11: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-14@2x.svg",
+    vector12: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-189@2x.svg",
+    vector13: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-190@2x.svg",
+    vector14: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-17@2x.svg",
+    vector15: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-192@2x.svg",
+    vector16: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-193@2x.svg",
+    vector17: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-20@2x.svg",
+    vector18: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-195@2x.svg",
+    vector19: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-196@2x.svg",
+    vector20: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-23@2x.svg",
+    vector21: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-198@2x.svg",
+    vector22: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-25@2x.svg",
+    vector23: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-26@2x.svg",
+    vector24: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-201@2x.svg",
+    vector25: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-202@2x.svg",
+    vector26: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-203@2x.svg",
+    vector27: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-30@2x.svg",
+    vector28: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-205@2x.svg",
+    group: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-32@2x.svg",
+    vector29: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-207@2x.svg",
+    vector30: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-34@2x.svg",
+    vector31: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-35@2x.svg",
+    vector32: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-34@2x.svg",
+    vector33: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-211@2x.svg",
+    vector34: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-212@2x.svg",
+    vector35: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-35@2x.svg",
+    vector36: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-35@2x.svg",
+    vector37: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-215@2x.svg",
+    vector38: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-216@2x.svg",
+    vector39: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-217@2x.svg",
+    vector40: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-44@2x.svg",
+    vector41: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-219@2x.svg",
+    transparency: "Transparency",
+    vector42: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-179@2x.svg",
+    vector43: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-6@2x.svg",
+    cost: "Cost",
+    rectangle77: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-77@1x.png",
+    howItWorks: "How it Works?",
+    text2: "There are many variations of passages of Lorem Ipsum available but the majority have suffered dummy alteration in that an some form by the injected humour or randomised words which don’t look even that slightly believable. If you are Lorem Ipsums your need to be sure there isn’t anything embarrassing. There are many variations passages of Lorem as Ipsums available but the majority have suffered that alterations in that some form by injected humour or randomised words which don’t looks even slightly that believable. If you are Lorem as Ipsum  you need to be sure there isn’t anything as embarrassing. There are many variation of passages Lorem Ipsum available but the majority have suffered that a aalteration in that some form by injected humour or randomised words which don’t look even slightly the believable. If you are Lorem Ipsum you need to be sure there is not the anything of embarrassing. If you are Lorem Ipsums your need to be sure there as isn’t anything embarrassing.",
+    rectangle48: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-48-6@1x.png",
+    rectangle49: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-49-6@1x.png",
+    rectangle50: "https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/rectangle-50-6@1x.png",
+    place2: "Quality",
+    text3: <>Quality for us is not just using expensive brands of raw materials, it mainly involves three key points, <br />Right design. By understanding the geological condition of your site as a prerequisite, we provide the customized design for your homesite by adopting inputs from the soil testing that we perform for each construction site.  <br />Following the right standard operating procedures. Our SOPs ensure we achieve high efficiency, quality outputs, and uniformity of performance.<br />Specific brands of construction material to be used for specific conditions.  Each brand of construction material has its own strengths and weaknesses. We provide suggestions at each stage for specific brands and types of material to be used according to your unique condition.</>,
+    transparenccy: "Transparenccy",
+    text4: "We possess third generation business continuity which is a rarity. Which means that you gain decades of collaborative experience to your benefit. Expertise, quality, transparency, reliability and cost effectiveness are why our customers come back to us again and again. We offer a perfect blend of all these factors resulting in deep customer delight. We put the “Smart” in everything that we do. Smarter design, smarter spaces, smarter value. Simply: smarter living. With exciting options and construction plans to choose from, one is certain to be right for you. Yes, other people do some of what we do, but no one does everything in the unique way we do.",
+    cost2: "Cost",
+    text5: "When it comes to building a new residential or commercial building, choosing the right partner to bring your vision to life is an important first step. Our experienced Webuildd team has all the expertise, resources, and connections you need to turn inspiration into reality with seamless ease. You don’t have to deal with the hassles of managing complex coordination and multiple resources. Our in-house team has you covered from legal, estimation, site planning, architectural design,",
+    group89Props: group89Data,
+    founderCardProps: founderCardData,
+    founderCard2Props: founderCard2Data,
+    founderCard32Props: founderCard32Data,
+    founderCard4Props: founderCard4Data,
+};
+
---
+++ components/Frame173/index.jsx
@@ -0,0 +1,233 @@
+import React from "react";
+import Group89 from "../Group89";
+import FounderCard from "../FounderCard";
+import FounderCard2 from "../FounderCard2";
+import FounderCard32 from "../FounderCard32";
+import FounderCard4 from "../FounderCard4";
+import "./Frame173.css";
+
+function Frame173(props) {
+  const {
+    text1,
+    rectangle76,
+    title,
+    whyChooseUs,
+    founders,
+    line55,
+    vector2,
+    vector3,
+    place,
+    vector4,
+    vector5,
+    vector6,
+    vector7,
+    vector8,
+    vector9,
+    vector10,
+    vector11,
+    vector12,
+    vector13,
+    vector14,
+    vector15,
+    vector16,
+    vector17,
+    vector18,
+    vector19,
+    vector20,
+    vector21,
+    vector22,
+    vector23,
+    vector24,
+    vector25,
+    vector26,
+    vector27,
+    vector28,
+    group,
+    vector29,
+    vector30,
+    vector31,
+    vector32,
+    vector33,
+    vector34,
+    vector35,
+    vector36,
+    vector37,
+    vector38,
+    vector39,
+    vector40,
+    vector41,
+    transparency,
+    vector42,
+    vector43,
+    cost,
+    rectangle77,
+    howItWorks,
+    text2,
+    rectangle48,
+    rectangle49,
+    rectangle50,
+    place2,
+    text3,
+    transparenccy,
+    text4,
+    cost2,
+    text5,
+    group89Props,
+    founderCardProps,
+    founderCard2Props,
+    founderCard32Props,
+    founderCard4Props,
+  } = props;
+
+  return (
+    <div className="frame-173">
+      <div className="overlap-group">
+        <Group89
+          line4={group89Props.line4}
+          line5={group89Props.line5}
+          line6={group89Props.line6}
+          line7={group89Props.line7}
+          line8={group89Props.line8}
+        />
+        <p className="text-1 typographybody16-regular">{text1}</p>
+        <img className="rectangle-76" src={rectangle76} />
+        <h1 className="title valign-text-middle typographyheadlineh2-extrabold-40">{title}</h1>
+        <div className="why-choose-us valign-text-middle typographyheadlineh2-extrabold-40">{whyChooseUs}</div>
+        <div className="founders valign-text-middle typographyheadlineh2-extrabold-40">{founders}</div>
+        <FounderCard
+          rectangle90={founderCardProps.rectangle90}
+          name={founderCardProps.name}
+          designation={founderCardProps.designation}
+        />
+        <FounderCard2
+          rectangle90={founderCard2Props.rectangle90}
+          name={founderCard2Props.name}
+          designation={founderCard2Props.designation}
+        />
+        <FounderCard32
+          rectangle90={founderCard32Props.rectangle90}
+          name={founderCard32Props.name}
+          designation={founderCard32Props.designation}
+        />
+        <FounderCard4
+          rectangle90={founderCard4Props.rectangle90}
+          name={founderCard4Props.name}
+          designation={founderCard4Props.designation}
+        />
+        <div className="group-154">
+          <div className="overlap-group1">
+            <img className="line-55" src={line55} />
+            <div className="group-151">
+              <div className="group-14 border-1px-red">
+                <div className="quality">
+                  <div className="overlap-group2-2">
+                    <img
+                      className="vector-18"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/60c1b397ba70f9d1bdda22ff/img/vector-176@2x.svg"
+                    />
+                    <img className="vector-38" src={vector2} />
+                    <img className="vector-5" src={vector3} />
+                  </div>
+                </div>
+              </div>
+              <div className="place valign-text-middle typographyheadlineh4-extrabold-24">{place}</div>
+            </div>
+            <div className="group-152">
+              <div className="group-150 border-1px-red">
+                <div className="overlap-group2-1">
+                  <div className="group-2">
+                    <div className="overlap-group4">
+                      <img className="vector-39" src={vector4} />
+                      <img className="vector-27" src={vector5} />
+                      <img className="vector-22" src={vector6} />
+                      <img className="vector-23" src={vector7} />
+                      <img className="vector-14" src={vector8} />
+                      <img className="vector-34" src={vector9} />
+                      <img className="vector-29" src={vector10} />
+                      <img className="vector-32" src={vector11} />
+                      <img className="vector-16" src={vector12} />
+                      <img className="vector-17" src={vector13} />
+                      <img className="vector-4" src={vector14} />
+                      <img className="vector-1" src={vector15} />
+                      <img className="vector-37" src={vector16} />
+                      <img className="vector-15" src={vector17} />
+                      <img className="vector-35" src={vector18} />
+                      <img className="vector-19" src={vector19} />
+                      <img className="vector-31" src={vector20} />
+                      <img className="vector-24" src={vector21} />
+                    </div>
+                    <div className="flex-row">
+                      <img className="vector-42" src={vector22} />
+                      <img className="vector-13" src={vector23} />
+                    </div>
+                  </div>
+                  <img className="vector-36" src={vector24} />
+                  <img className="vector-11" src={vector25} />
+                  <img className="vector-40" src={vector26} />
+                  <img className="vector-8" src={vector27} />
+                  <img className="vector-7" src={vector28} />
+                  <div className="group-1" style={{ backgroundImage: `url(${group})` }}></div>
+                  <img className="vector-41" src={vector29} />
+                </div>
+                <div className="group">
+                  <div className="overlap-group5">
+                    <img className="vector-33" src={vector30} />
+                    <img className="vector-12" src={vector31} />
+                    <img className="vector-2" src={vector32} />
+                    <img className="vector-30" src={vector33} />
+                    <img className="vector-20" src={vector34} />
+                    <img className="vector" src={vector35} />
+                    <img className="vector-6" src={vector36} />
+                  </div>
+                </div>
+                <div className="overlap-group3">
+                  <img className="vector-28" src={vector37} />
+                  <img className="vector-25" src={vector38} />
+                  <img className="vector-10" src={vector39} />
+                  <img className="vector-3" src={vector40} />
+                  <img className="vector-9" src={vector41} />
+                </div>
+              </div>
+              <div className="transparency valign-text-middle typographyheadlineh4-extrabold-24">{transparency}</div>
+            </div>
+          </div>
+          <div className="group-153">
+            <div className="group-14 border-1px-red">
+              <div className="money-bag">
+                <div className="overlap-group2">
+                  <img className="vector-21" src={vector42} />
+                  <img className="vector-26" src={vector43} />
+                </div>
+              </div>
+            </div>
+            <div className="cost-1 valign-text-middle typographyheadlineh4-extrabold-24">{cost}</div>
+          </div>
+        </div>
+        <div className="frame-171">
+          <img className="rectangle-77" src={rectangle77} />
+          <div className="group-155">
+            <div className="how-it-works valign-text-middle typographyheadlineh2-extrabold-40">{howItWorks}</div>
+            <p className="text-2 typographybody16-regular">{text2}</p>
+          </div>
+        </div>
+        <img className="rectangle-48" src={rectangle48} />
+        <img className="rectangle-49" src={rectangle49} />
+        <img className="rectangle-50" src={rectangle50} />
+        <div className="frame-29-2">
+          <div className="place-1 valign-text-middle overpass-extra-bold-shark-32px">{place2}</div>
+          <p className="text-3 typography-body-smalltext-14-regular">{text3}</p>
+        </div>
+        <div className="frame-29">
+          <div className="transparenccy valign-text-middle overpass-extra-bold-shark-32px">{transparenccy}</div>
+          <p className="text-4 typography-body-smalltext-14-regular">{text4}</p>
+        </div>
+        <div className="frame-29-1">
+          <div className="cost valign-text-middle overpass-extra-bold-shark-32px">{cost2}</div>
+          <p className="text-5 typography-body-smalltext-14-regular">{text5}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Frame173;
